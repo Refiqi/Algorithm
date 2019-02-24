46adfab3a4a4c8446bbd39dbb2ee42f6068a6176
refiqi <refyfadhila96@gmail.com>
2019-02-24 10:21:58 +0700

    Recursive Steps

--- exercise/steps.rb
+++ exercise/steps.rb
@@ -17,21 +17,51 @@
 #       '### '
 #       '####'
 
-def steps num
-    stair = String.new
+# Recursive Method
 
-    (0...num).each do |x|
-        (0...num).each do |y|
-            if y <= x
-                stair += '#'
-            else
-                stair += '*'
-            end
-        end
+def steps (n, row = 0, stair = '')
+    return if n == row
 
+    if n == stair.length
         puts stair
+        return steps(n, row + 1)
     end
 
+    if stair.length <= row
+        stair += '#'
+    else
+        stair += ' '
+    end
+
+    steps(n, row, stair)
 end
+
+
+
+
+
+
+
+
+
+
+
+## Iterate method
+# def steps num
+#     stair = String.new
+
+#     (0...num).each do |x|
+#         (0...num).each do |y|
+#             if y <= x
+#                 stair += '#'
+#             else
+#                 stair += '*'
+#             end
+#         end
+
+#         puts stair
+#     end
+
+# end
 
 steps(5)
